db8e37a49d8efa79b512e923ee3daeba87e16e30
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2024-02-08 15:53:28 +0530

    Update td-carousal.js

--- td-carousal.js
+++ td-carousal.js
@@ -1,115 +1,178 @@
 function tdcarousal({
-    slideElement,
-    slideNextBtn,
-    slidePrevBtn,
-    indicator,
-}) {
-    let items = document.querySelectorAll(slideElement);
+  wrapperElement,
+  slideElement,
+  slideNextBtn,
+  slidePrevBtn,
+  totalSlideCount,
+  currentSlideCount,
+  indicator,
+}){
+    let wrapper = document.querySelector(wrapperElement);
+    let items = wrapper.querySelectorAll(slideElement);
     let nextBtn = document.querySelector(slideNextBtn);
     let prevBtn = document.querySelector(slidePrevBtn);
     let navigation_indicator = document.querySelector(indicator);
-    let totalSlide = document.querySelector('.totalSlide');
-    let currentSlide = document.querySelector('.currentSlide');
+    let totalSlide = document.querySelector(totalSlideCount);
+    let currentSlide = document.querySelector(currentSlideCount);
     let indicators_array;
     var state = 0;
-    /* iife */
-    (function init() {
-        items.forEach((item, index) => {
-            item.classList.remove('td-active-p');
-            item.classList.remove('td-active');
-            item.classList.remove('td-active-n');
-            let nav_light = document.createElement('span');
-            nav_light.classList.add('nav_light');
-            navigation_indicator.appendChild(nav_light);
-        })
-        state = 1;
-        totalSlide.textContent = items.length;
-        setCurrentSlide(state);
-        indicatorGlow(state);
-        (items[state - 1] !== undefined) ? items[state - 1].classList.add('td-active-p'): '';
-        items[state].classList.add('td-active');
-        (items[state + 1] !== undefined) ? items[state + 1].classList.add('td-active-n'): '';
+      /* iife */
+    (function init(){
+     let styleCustom = document.createElement('div');
+      styleCustom.innerHTML  =  styleCustom.innerHTML +`
+      .td-active-${wrapperElement}{
+        scale:1;
+        z-index:10;
+         filter:grayscale(0);
+        & img{
+          width:100%;
+          height:100%;
+        }
+      }
+      .td-active-p${wrapperElement}{
+        scale:.7;
+        transform-origin:center;
+        translate:-60%;
+        z-index:1;
+         & img{
+          width:100%;
+          height:100%;
+        }
+      }
+      .td-active-n${wrapperElement}{
+        scale:.7;
+        transform-origin:center;
+        translate:60%;
+        z-index:1;
+         & img{
+          width:100%;
+          height:100%;
+        }
+  }
+      `;
+    
+    items.forEach((item,index)=>{
+      item.classList.remove('td-active-p');
+      item.classList.remove('td-active');
+      item.classList.remove('td-active-n');
+      navigation_indicator.setAttribute('min','0');
+      navigation_indicator.setAttribute('max',items.length-1);
+      navigation_indicator.value=state;
+     }) 
+    state=1;
+      totalSlide.textContent=items.length;
+      setCurrentSlide(state);
+      indicatorGlow(state);
+    (items[state-1] !== undefined) ? items[state-1].classList.add('td-active-p'):'';
+    items[state].classList.add('td-active');
+    (items[state+1]!== undefined) ? items[state+1].classList.add('td-active-n'):''; 
     })();
-    /* iife */
-    function indicatorGlow(state) {
-
-        for (let i = 0; i < items.length; i++) {
-            navigation_indicator.children[i].classList.remove('indicate');
-        }
-        navigation_indicator.children[state].classList.add('indicate');
+      /* iife */
+    function indicatorGlow(state){
+      
+      navigation_indicator.value=state;
+     /* for(let i=0;i<items.length;i++){
+        navigation_indicator.children[i].classList.remove('indicate');
+      }
+      navigation_indicator.children[state].classList.add('indicate');*/
     }
-
-    function setCurrentSlide(state) {
-        currentSlide.textContent = state + 1;
+  
+    function setCurrentSlide(state){
+      currentSlide.textContent="";
+      currentSlide.textContent=parseInt(state) + 1;
     }
-
-    function nextfinal() {
-        items.forEach((item) => {
+    function nextfinal(){
+          items.forEach((item)=>{
             item.classList.remove('td-active-p');
             item.classList.remove('td-active');
             item.classList.remove('td-active-n');
-        })
+          })
 
-        state = 0;
-        (items[state - 1] !== undefined) ? items[state - 1].classList.add('td-active-p'): '';
-        items[state].classList.add('td-active');
-        (items[state + 1] !== undefined) ? items[state + 1].classList.add('td-active-n'): '';
+      state=0;
+          (items[state-1] !== undefined) ? items[state-1].classList.add('td-active-p'):'';
+          items[state].classList.add('td-active');
+          (items[state+1]!== undefined) ? items[state+1].classList.add('td-active-n'):'';
 
     }
-
-    function prevfinal() {
-        items.forEach((item) => {
+    function prevfinal(){
+          items.forEach((item)=>{
             item.classList.remove('td-active-p');
             item.classList.remove('td-active');
             item.classList.remove('td-active-n');
-        })
+          })
 
-        state = items.length - 1;
+      state = items.length-1;
 
-        (items[state - 1] !== undefined) ? items[state - 1].classList.add('td-active-p'): '';
-        items[state].classList.add('td-active');
-        (items[state + 1] !== undefined) ? items[state + 1].classList.add('td-active-n'): '';
+          (items[state-1] !== undefined) ? items[state-1].classList.add('td-active-p'):'';
+          items[state].classList.add('td-active');
+          (items[state+1]!== undefined) ? items[state+1].classList.add('td-active-n'):'';
 
     }
-
-    function next() {
-
-        if (state < items.length - 1) {
-            (items[state - 1] !== undefined) ? items[state - 1].classList.remove('td-active-p'): '';
-            items[state].classList.remove('td-active');
-            (items[state + 1] !== undefined) ? items[state + 1].classList.remove('td-active-n'): '';
-            state = state + 1;
-            (items[state - 1] !== undefined) ? items[state - 1].classList.add('td-active-p'): '';
-            items[state].classList.add('td-active');
-            (items[state + 1] !== undefined) ? items[state + 1].classList.add('td-active-n'): '';
-        } else {
-            nextfinal();
-        }
-        indicatorGlow(state);
-        setCurrentSlide(state);
+    function next(){
+      
+      if(state < items.length-1){
+          (items[state-1] !== undefined)?items[state-1].classList.remove('td-active-p'):'';
+          items[state].classList.remove('td-active');
+          (items[state+1]!== undefined) ? items[state+1].classList.remove('td-active-n'):'';
+          state=state+1;
+          (items[state-1] !== undefined)?items[state-1].classList.add('td-active-p'):'';
+          items[state].classList.add('td-active');
+          (items[state+1]!== undefined) ? items[state+1].classList.add('td-active-n'):'';
+      }
+      else{
+         nextfinal();
+      }
+      indicatorGlow(state);
+      setCurrentSlide(state);
     }
+    function prev(){
 
-    function prev() {
-
-        if (state > 0) {
-            (items[state - 1] !== undefined) ? items[state - 1].classList.remove('td-active-p'): '';
-            items[state].classList.remove('td-active');
-            (items[state + 1] !== undefined) ? items[state + 1].classList.remove('td-active-n'): '';
-            state = state - 1;
-            (items[state - 1] !== undefined) ? items[state - 1].classList.add('td-active-p'): '';
-            items[state].classList.add('td-active');
-            (items[state + 1] !== undefined) ? items[state + 1].classList.add('td-active-n'): '';
-        } else {
-            prevfinal();
-        }
-        indicatorGlow(state);
-        setCurrentSlide(state);
+      if(state >0){
+          (items[state-1] !== undefined) ? items[state-1].classList.remove('td-active-p'):'';
+          items[state].classList.remove('td-active');
+          (items[state+1]!== undefined) ? items[state+1].classList.remove('td-active-n'):'';
+          state=state-1;
+          (items[state-1] !== undefined) ? items[state-1].classList.add('td-active-p'):'';
+          items[state].classList.add('td-active');
+           (items[state+1]!== undefined) ? items[state+1].classList.add('td-active-n'):'';
+      }
+      else{
+         prevfinal();
+      }
+      indicatorGlow(state);
+      setCurrentSlide(state);
     }
+    
+    function stateSlider(currState){
+      let i;
+      for(i=0;i<items.length;i++){
+            items[i].classList.remove('td-active-p');
+            items[i].classList.remove('td-active');
+            items[i].classList.remove('td-active-n');
+      }
+     
+      state=parseInt(currState);
+      console.log(currState);
+            (items[state-1] !== undefined) ? items[state-1].classList.add('td-active-p'):'';
+          items[state].classList.add('td-active');
+           (items[state+1]!== undefined) ? items[state+1].classList.add('td-active-n'):'';
+    }
+  
+  
+    nextBtn.addEventListener('click',()=>{next()});
+    prevBtn.addEventListener('click',()=>{prev()});
+    navigation_indicator.addEventListener('input',(e)=>{
+      stateSlider(e.target.value);
+      setCurrentSlide(e.target.value);
+    })
+  }
 
-    nextBtn.addEventListener('click', () => {
-        next()
-    });
-    prevBtn.addEventListener('click', () => {
-        prev()
-    });
-}
+var tdcarousal = tdcarousal({
+  wrapperElement:".td-carousel-wrapper",
+  slideElement:".td-item",
+  slideNextBtn:".td-btn-next",
+  slidePrevBtn:".td-btn-prev",
+  totalSlideCount:".totalSlide",
+  currentSlideCount:".currentSlide",
+  indicator:".indicator",
+}); 
